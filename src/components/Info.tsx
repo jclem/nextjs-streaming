3605ae9e6c8a4348eae509d7d0a3ad397f4c6bcf
Jonathan Clem <jonathan@jclem.net>
2021-11-11 15:25:11 +0000

    Add more clarification

--- src/components/Info.tsx
+++ src/components/Info.tsx
@@ -1,5 +1,5 @@
-import {VFC} from 'react'
 import Link from 'next/link'
+import {VFC} from 'react'
 
 const Info: VFC = () => (
   <>
@@ -9,22 +9,51 @@ const Info: VFC = () => (
     </p>
 
     <p>
+      Source:{' '}
+      <a href="https://github.com/jclem/nextjs-streaming">
+        jclem/nextjs-streaming
+      </a>
+    </p>
+
+    <p>
       <Link href="/lazy">
         <a>The /lazy route</a>
       </Link>{' '}
-      uses <code>React.lazy</code> to load a the slow-fetch component. Note that
-      the browser's loading indicator stops before the component un-suspends.
+      uses <code>React.lazy</code> to load a the slow-fetch component.
     </p>
+
+    <ul>
+      <li>
+        <p>
+          In development, the browser's loading indicator stops before the
+          component un-suspends and the button is interactive before the slow
+          fetch is ready.
+        </p>
+      </li>
+      <li>
+        <p>
+          In production, the loading indicator continues until the component
+          un-suspends and the button is not interactive until then.
+        </p>
+      </li>
+    </ul>
 
     <p>
       <Link href="/nonlazy">
         <a>The /nonlazy route</a>
       </Link>{' '}
-      is a regular component import. Note that there is apparently no selective
-      hydration happening (the counter is interactive but not hydrated).
-      However, in this case, the browser loading indicator <em>does</em>{' '}
-      continue to run until the slow-fetch component un-suspends.
+      is a regular component import of the slow fetch component.
     </p>
+
+    <ul>
+      <li>
+        <p>
+          In development <em>and</em> production, the loading indicator
+          continues until the component un-suspends and the button is not
+          interactive until then.
+        </p>
+      </li>
+    </ul>
   </>
 )
 
